599ab404e95828394a3b47ea71be2ebe60fbfa6c
Fadhil Amadan <fadhil.amadan@yahoo.co.id>
2023-05-10 15:17:50 +0700

    refactor(script): add style with display block

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const remoteToggle = document.getElementById('remote-toggle');
 
 if (remoteToggle !== null && remoteToggle !== undefined) {
+  remoteToggle.setAttribute('style', 'display: block !important;');
   remoteToggle.setAttribute('display', 'block');
 }
